b1369691794ae9ab64671e65b51d3464fa7f37fa
Alexander Zhuravlev <a-798@mail.ru>
2024-02-12 14:22:43 +0300

    add Lesson_3

---
+++ Lesson_3/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+void sort(std::vector<int>& numbers, int size, int start, std::promise<int>& prom)
+{
+    int temp = 0;
+    int min = numbers[start];
+    for (int i = start; i < size; i++)
+    {
+        if (numbers[i] <= min)
+        {
+            temp = i;
+            min = numbers[i];
+        }            
+    }
+    prom.set_value(temp);
+}
+
+
+int main()
+{
+    srand(time(0));
+    
+    std::vector<int> numbers;
+    int n;
+    int k = 0;
+    int w = 0;
+
+    std::cout << "Enter size: ";
+    std::cin >> n;
+    numbers.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        numbers[i] = 0 + rand() % 11;
+    }
+
+
+    for (auto& t : numbers)
+    {
+        std::cout << t << " ";
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        std::promise<int> prom; //переопределяем переменные по новой, чтобы обновлялась связь между фьючер и промис, иначе пееременная k будет одним и тем же значением каждую новую итерацию
+        std::future<int> f = prom.get_future();
+
+        auto ff = std::async(sort, std::ref(numbers), n, i, std::ref(prom));
+        k = f.get();
+        w = numbers[k];
+        numbers[k] = numbers[i];
+        numbers[i] = w;
+    }
+
+
+    std::cout << std::endl;
+    for (auto& t : numbers)
+    {
+        std::cout << t << " ";
+    }
+    
+
+    return 0;
+}
---
+++ Lesson_3/ConsoleApplication1_Task_2/ConsoleApplication1_Task_2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+std::string func()
+{
+    return "do something in func";
+}
+
+template <class T, class TT>
+std::string my_for_each(T begin, T end, TT func)
+{
+    auto size = std::distance(begin, end);
+    if (size <= 2) //делим на 3 блока, т.е. будет по 2 эл-та в каждом
+    {
+        return func();
+    }
+    auto mid = begin;
+    std::advance(mid, size / 2);
+    auto f1 = std::async(my_for_each<T, TT>, begin, mid, func);
+    auto f2 = my_for_each(mid, end, func);
+    return f1.get() + "  +  " + f2;
+}
+
+
+int main()
+{
+    std::vector<int> my_vector{ 7, 2, 9, 3, 5, 1, 4, 0 };
+    int y = 0;
+
+    std::promise<int> prom; 
+    auto f = prom.get_future();
+
+    std::cout << my_for_each(my_vector.begin(), my_vector.end(), func);
+
+    return 0;
+}
+
+//                       7 2 9 3 5 1 4 0
+//                             /\
+//                            /  \
+//                           /    \
+//                          /      \
+//                         /        \
+//                    7 2 9 3  +  5 1 4 0     
+//                        /\         /\
+//                       /  \       /  \
+//                    7 2   9 3   5 1  4 0
+//                   (do + do)   (do  +  do)
