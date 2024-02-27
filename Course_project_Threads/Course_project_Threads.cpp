c67a11dd39e6b588a75c781fb22640e5aaa377be
kalashnikov.v24 <kalashnikov.v24@wb.ru>
2024-02-28 01:02:05 +0300

    review

--- Course_project_Threads/Course_project_Threads.cpp
+++ Course_project_Threads/Course_project_Threads.cpp
@@ -6,7 +6,6 @@
 #include <queue>
 #include <condition_variable>
 
-
 void func1()
 {
     std::cout << "Working: " << __FUNCTION__ << " " << std::endl;
@@ -17,13 +16,13 @@ void func2()
     std::cout << "Working: " << __FUNCTION__ << std::endl;
 }
 
-
 class safe_queue
 {
 public:
-    std::mutex temp_mutex2;
+    // TODO все эти атрибуты должны быть скрыты в private!
+    std::mutex temp_mutex2; // TODO откуда такие названия? Можно же просто назвать std::mutex m
     std::condition_variable data_cond;
-    std::queue<std::function<void()>> my_q;
+    std::queue<std::function<void()>> my_q; // TODO откуда такие названия? Можно же просто назвать q или que
 
     void push(std::function<void()> func)
     {
@@ -31,7 +30,9 @@ public:
         my_q.push(func);
         data_cond.notify_all();
     }
-
+    // TODO здесь в front и pop присутствует API race. Нужно объединить в один метод try_pop()
+    //  и при возврате значений из метода использовать условные переменные data_cond
+    //  для ожидания того что в очереди есть что вернуть
     std::function<void()> front()
     {
         std::lock_guard<std::mutex> lock_guard(temp_mutex2);
@@ -45,17 +46,15 @@ public:
     }
 };
 
-
-
-class thread_pool : public safe_queue
+class thread_pool : public safe_queue // TODO что это??? У вас уже есть композиция, зачем еще наследование?! Bad design
 {
 private:
     std::vector<std::thread> threads_pool;
-    safe_queue temp_safe_queue;
-    std::mutex temp_mutex1;
+    safe_queue temp_safe_queue; // TODO Откуда такой дикий нейминг? Можно же просто que
+    std::mutex temp_mutex1;     // TODO bad naming
 
 public:
-    thread_pool(int cores) : threads_pool(cores)
+    thread_pool(int cores) : threads_pool(cores) // TODO почему int? И что в этом случае будет если вам передадут cores < 0?
     {
         threads_pool[0] = std::thread(&thread_pool::submit, this, func1);
         threads_pool[1] = std::thread(&thread_pool::submit, this, func2);
@@ -68,8 +67,10 @@ public:
 
     ~thread_pool()
     {
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; i++) // TODO откуда в цикле берется число 8? должно быть все параметризировано
         {
+            // TODO вы никогда не дождетесь этих джоинов.
+            // Нужно помимо джоинов здесь предусмотреть механизм оповещения выхода из вечных циклов
             threads_pool[i].join();
         }
     }
@@ -78,10 +79,19 @@ public:
     {
         while (true)
         {
-            std::unique_lock<std::mutex>  lock_guard(temp_mutex2);
-            data_cond.wait(lock_guard, [&]() {
-                return !temp_safe_queue.my_q.empty();
-                });
+            /*TODO
+            1. нужно оповещение для выхода из вечного цикла, например с помощью атомарного булевого флага
+            2. Блокировку при возврате значений делать в методе try_pop у safe_queue используя data_cond
+            3. Должно выглядеть так:
+             if (exit) {return};
+             auto func = que.try_pop();
+             std::lock_guard lk(m);
+             func();
+            */
+            // TODO убрать все что следует ниже
+            std::unique_lock<std::mutex> lock_guard(temp_mutex2);
+            data_cond.wait(lock_guard, [&]()
+                           { return !temp_safe_queue.my_q.empty(); });
             std::cout << std::this_thread::get_id() << std::endl;
             auto temp_func = temp_safe_queue.front();
             temp_func();
@@ -91,18 +101,16 @@ public:
     }
 
     void submit(std::function<void()> func)
-    {   
+    {
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         for (int i = 0; i < 3; i++)
         {
             std::lock_guard<std::mutex> lock_guard(temp_mutex1);
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
-            temp_safe_queue.push(func);           
+            temp_safe_queue.push(func);
         }
     }
 };
-
-
 
 int main()
 {
@@ -111,10 +119,6 @@ int main()
     std::cout << "Cores: " << cores << std::endl;
     return 0;
 }
-
-
-
-
 
 /*
 
